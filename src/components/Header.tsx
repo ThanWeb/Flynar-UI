f407826088c60cd062a384b25baa95908d933f26
faurrahman10 <faturrahmanhasir@gmail.com>
2023-06-16 20:17:22 +0800

    membuat home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,20 +15,21 @@ const Header = (): ReactElement => {
               <Image src='/assets/logoTiketku.png' width={98} height={53} alt='' className='img-fluid' style={{ marginRight: '34px' }} />
               <form className='d-flex input-group'>
                 <input
+                  className='border-0'
                   type='text'
                   placeholder='Cari di sini..'
                   style={{
-                    width: '444px',
+                    width: '400px',
                     height: '48px',
                     color: '#000',
+                    backgroundColor: '#EEEEEE',
                     padding: '12px 16px',
                     outline: 'none',
-                    border: '1px solid #D0D0D0',
-                    borderRadius: '16px'
+                    borderRadius: '16px 0 0 16px'
                   }}
                 />
-                <button className='border-0' style={{ backgroundColor: 'transparent' }} type='submit'>
-                  <FontAwesomeIcon icon={faMagnifyingGlass} className='text-black-50'/>
+                <button className='border-0' style={{ backgroundColor: '#EEEEEE', border: '1px solid #D0D0D0', borderRadius: '0 16px 16px 0' }} type='submit'>
+                  <FontAwesomeIcon icon={faMagnifyingGlass} className='text-black-50 pe-3'/>
                 </button>
               </form>
             </div>
